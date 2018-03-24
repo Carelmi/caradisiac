6a06aaf4b4e1429022e6cb397a41aa45a365926f
carel <elimbicarel@gmail.com>
2018-03-24 13:50:16 +0100

    (index) Creating the server in order to upload data ti the elasticsearch database

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const {getBrands} = require('node-car-api');
 const {getModels} = require('node-car-api');
+var elasticsearch= require('elasticsearch');
+var body= []
+var client = new elasticsearch.Client({
+     host: 'localhost:9200',
+     log: 'trace'
+ });
 
-async function print () {
-  const brands = await getBrands();
 
-  console.log(brands);
+
+
+async function GetAll(){
+  let j=0;
+  var brands = await getBrands();
+  for(let i=0; i < brands.length; i++){
+     var models= await getModels(brands[i]);
+     models.forEach(function(model){
+        var config = { index:  { _index: 'caradisia', _type: 'model', _id: j } };
+        body.push(config)
+        body.push(model)
+        j++
+     })
+  }
 }
 
-async function print () {
-  const models = await getModels('PEUGEOT');
-
-  console.log(models);
+function SaveDB(){
+  client.bulk({
+        body: body
+    }, function (error, response) {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        else {
+            console.log(response);  
+        }
+    });
 }
-
-print();
