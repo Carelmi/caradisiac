cd4e1db9eb95a70dfef6999ee1522399bca18aa9
carel <elimbicarel@gmail.com>
2018-03-24 14:22:18 +0100

    (index) the api

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const {getBrands} = require('node-car-api');
 const {getModels} = require('node-car-api');
 var elasticsearch= require('elasticsearch');
-var body= []
+var express = require('express');
+const app = express();
+var body= [];
 var client = new elasticsearch.Client({
      host: 'localhost:9200',
      log: 'trace'
  });
 
+ const port = 9292;
+ app.listen(port, () => {
+   console.log('We are live on ' + port);
+ });
 
 
 
@@ -16,7 +22,7 @@ async function GetAll(){
   for(let i=0; i < brands.length; i++){
      var models= await getModels(brands[i]);
      models.forEach(function(model){
-        var config = { index:  { _index: 'caradisia', _type: 'model', _id: j } };
+        var config = { index:  { _index: 'caradisiac', _type: 'model', _id: j } };
         body.push(config)
         body.push(model)
         j++
@@ -33,7 +39,35 @@ function SaveDB(){
             return;
         }
         else {
-            console.log(response);  
+            console.log(response);
         }
     });
-}
+  }
+
+
+  app.get('/populate', function (req, res) {
+      SaveDB();
+      res.send('SUCCESS!')
+  })
+
+  app.get('/cars', function(req,res){
+
+      client.search({
+          index: 'caradisiac',
+          type: 'model',
+          body:{
+              "size":10,
+              "sort":[
+                  {"volume.keyword" :{"order":"desc"}}
+              ]
+          }
+      },function (error, response,status) {
+          if (error){
+              console.log("ERROR : "+error)
+          }
+          else {
+              res.send(response.hits.hits);
+          }
+      });
+
+  })
